2f0512e04fde4b16ee769b08daa4f5f8499e909b
ErGk <eren.gdk10@gmail.com>
2022-04-29 12:36:27 +0300

    Update and rename diziler/connect.php to diger/database-connect.php

--- diger/database-connect.php
+++ diger/database-connect.php
@@ -7,4 +7,13 @@
 	// Create connection
 	$mysqlicon = new mysqli($servername, $username, $password, $dbname);
 	$mysqlicon->set_charset("utf8");
+
+// PDO
+
+ try{
+        $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+    }catch(PDOexception $e){
+        echo $e -> getMessage();
+    }
+
 ?>
